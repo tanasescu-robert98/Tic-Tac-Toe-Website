f9c2d220f994d1cf66f8bd4ffbb707de9081fcda
tanasescu-robert98 <80208723+tanasescu-robert98@users.noreply.github.com>
2021-09-22 18:40:18 +0300

    first commit

---
+++ website.js
@@ -0,0 +1,102 @@
+function myFunction() 
+{
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") 
+    {
+      x.style.display = "block";
+    } 
+    else 
+    {
+      x.style.display = "none";
+    }
+}
+
+function change(e1)
+{
+  if(e1.innerHTML == "0" || e1.innerHTML == "X")
+          return;    
+      if(GAME_OVER == 0)
+      {
+        N++;
+        
+        if(N % 2 == 0)
+        {
+          e1.innerHTML = "X";
+          e1.disabled = true;
+        }
+        else
+        {
+          e1.innerHTML = "0";
+        }
+        console.log(N);
+        check_endgame();
+      }
+}
+
+function check_endgame()
+{
+      var button1 = document.getElementById("button1");
+      var button2 = document.getElementById("button2");
+      var button3 = document.getElementById("button3");
+      var button4 = document.getElementById("button4");
+      var button5 = document.getElementById("button5");
+      var button6 = document.getElementById("button6");
+      var button7 = document.getElementById("button7");
+      var button8 = document.getElementById("button8");
+      var button9 = document.getElementById("button9");
+      if(button1.innerHTML == button2.innerHTML && button1.innerHTML == button3.innerHTML && button2.innerHTML == button3.innerHTML && (button1.innerHTML == "X" || button1.innerHTML == "0")) 
+      {
+        console.log("Player 1 WON!!!");
+        GAME_OVER = 1;
+        return;
+      }
+      if(button4.innerHTML == button5.innerHTML && button4.innerHTML == button6.innerHTML && button5.innerHTML == button6.innerHTML && (button4.innerHTML == "X" || button4.innerHTML == "0")) 
+      {
+        console.log("Player 1 WON!!!");
+        GAME_OVER = 1;
+        return;
+      }
+      if(button7.innerHTML == button8.innerHTML && button7.innerHTML == button9.innerHTML && button8.innerHTML == button9.innerHTML && (button7.innerHTML == "X" || button7.innerHTML == "0")) 
+      {
+        console.log("Player 1 WON!!!");
+        GAME_OVER = 1;
+        return;
+      }
+      if(button1.innerHTML == button4.innerHTML && button1.innerHTML == button7.innerHTML && button4.innerHTML == button7.innerHTML && (button1.innerHTML == "X" || button1.innerHTML == "0")) 
+      {
+        console.log("Player 1 WON!!!");
+        GAME_OVER = 1;
+        return;
+      }
+      if(button2.innerHTML == button5.innerHTML && button2.innerHTML == button8.innerHTML && button5.innerHTML == button8.innerHTML && (button2.innerHTML == "X" || button2.innerHTML == "0")) 
+      {
+        console.log("Player 1 WON!!!");
+        GAME_OVER = 1;
+        return;
+      }
+      if(button3.innerHTML == button6.innerHTML && button3.innerHTML == button9.innerHTML && button6.innerHTML == button9.innerHTML && (button3.innerHTML == "X" || button3.innerHTML == "0")) 
+      {
+        console.log("Player 1 WON!!!");
+        GAME_OVER = 1;
+        return;
+      }
+      if(button1.innerHTML == button5.innerHTML && button1.innerHTML == button9.innerHTML && button5.innerHTML == button9.innerHTML && (button1.innerHTML == "X" || button1.innerHTML == "0")) 
+      {
+        console.log("Player 1 WON!!!");
+        GAME_OVER = 1;
+        return;
+      }
+      if(button7.innerHTML == button5.innerHTML && button7.innerHTML == button3.innerHTML && button5.innerHTML == button3.innerHTML && (button7.innerHTML == "X" || button7.innerHTML == "0")) 
+      {
+        console.log("Player 1 WON!!!");
+        GAME_OVER = 1;
+        return;
+      }
+}
+
+function f1(objButton)
+{
+    if (objButton.innerHTML=="EXPAND") objButton.innerHTML = "MINIMIZE";
+    else 
+      objButton.innerHTML = "EXPAND";
+}
