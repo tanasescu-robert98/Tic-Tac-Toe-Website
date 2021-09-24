c453aee4b5f389f72d0696cee9bafc706220cca7
tanasescu-robert98 <80208723+tanasescu-robert98@users.noreply.github.com>
2021-09-24 19:52:32 +0300

    stilizat cod

---
+++ website_themes.js
@@ -0,0 +1,8 @@
+function changeBodyBg()
+{
+    if(document.body.style.background == '')
+    document.body.style.background = 'powderblue';
+    else
+    document.body.style.background = '';
+    
+}
