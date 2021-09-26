4aef95bb4fcbbfb8a46d38d3faed4cbeecd7d015
tanasescu-robert98 <80208723+tanasescu-robert98@users.noreply.github.com>
2021-09-26 13:43:25 +0300

    test persistent index

--- persistent_data.js
+++ persistent_data.js
@@ -1,19 +1,30 @@
 function clickCounter() 
 {
-    if (typeof(Storage) !== "undefined") 
-    {
-      if (localStorage.clickcount != null) 
-      {
-        localStorage.clickcount = Number(localStorage.clickcount)+1;
-      } 
-      else 
-      {
-        localStorage.clickcount = 1;
-      }
-      document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
-    } 
-    else 
-    {
-      document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
-    }
+  var val1 = localStorage.getItem('value1');
+  if(val1 == null)
+  {
+    localStorage.setItem('value1', 1);
+  }
+  else
+  {
+    val1++;
+    localStorage.setItem('value1', val1);
+  }
+  console.log(val1);
+  //   if (typeof(Storage) !== "undefined") 
+  //   {
+  //     if (localStorage.clickcount != null) 
+  //     {
+  //       localStorage.clickcount = Number(localStorage.clickcount)+1;
+  //     } 
+  //     else 
+  //     {
+  //       localStorage.clickcount = 1;
+  //     }
+  //     document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
+  //   } 
+  //   else 
+  //   {
+  //     document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+  //   }
   }
--- website_themes.js
+++ website_themes.js
@@ -1,6 +1,22 @@
 function changeBodyBg()
 {
-    localStorage.clickcount = Number(localStorage.clickcount)+1;
+    var val1 = localStorage.getItem('value1');
+    if(val1 == null)
+    {
+        localStorage.setItem('value1', 1);
+    }
+    else
+    {
+        val1++;
+        localStorage.setItem('value1', val1);
+    }
+    console.log(val1);
+    //localStorage.clickcount = Number(localStorage.clickcount)+1;
+    // var val1 = localStorage.getItem('value1');
+    // val1++;
+    // console.log(val1);
+    // localStorage.setItem('value1', val1);
+    // console.log(val1);
     switch(document.body.style.background) 
     {
         case '':
