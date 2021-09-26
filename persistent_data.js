4c3867b6ec48bcfb54b7cbcc4c782d6b9d409227
tanasescu-robert98 <80208723+tanasescu-robert98@users.noreply.github.com>
2021-09-26 14:36:54 +0300

    fixed themes

--- persistent_data.js
+++ persistent_data.js
@@ -11,20 +11,4 @@ function clickCounter()
     localStorage.setItem('value1', val1);
   }
   console.log(val1);
-  //   if (typeof(Storage) !== "undefined") 
-  //   {
-  //     if (localStorage.clickcount != null) 
-  //     {
-  //       localStorage.clickcount = Number(localStorage.clickcount)+1;
-  //     } 
-  //     else 
-  //     {
-  //       localStorage.clickcount = 1;
-  //     }
-  //     document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
-  //   } 
-  //   else 
-  //   {
-  //     document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
-  //   }
-  }
+}
--- website_themes.js
+++ website_themes.js
@@ -3,7 +3,7 @@ function changeBodyBg()
     var val1 = localStorage.getItem('value1');
     if(val1 == null)
     {
-        localStorage.setItem('value1', 1);
+        localStorage.setItem('value1', 0);
     }
     else
     {
@@ -11,12 +11,6 @@ function changeBodyBg()
         localStorage.setItem('value1', val1);
     }
     console.log(val1);
-    //localStorage.clickcount = Number(localStorage.clickcount)+1;
-    // var val1 = localStorage.getItem('value1');
-    // val1++;
-    // console.log(val1);
-    // localStorage.setItem('value1', val1);
-    // console.log(val1);
     switch(document.body.style.background) 
     {
         case '':
